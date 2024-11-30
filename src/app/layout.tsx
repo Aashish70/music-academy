c2173046a2df8a446e5cd31536819fd4ce2c5d43
Aashish70 <ashishsingh70002@gmail.com>
2024-11-30 16:48:59 +0530

    Project completed

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import HeroSection from "@/components/HeroSection";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import TesimonialCards from "@/components/TesimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUS from "@/components/WhyChooseUS";
+
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUS />
+      <TesimonialCards />
+      <UpcomingWebinars />
+      <Instructors />
+    </main>
+  );
+}
